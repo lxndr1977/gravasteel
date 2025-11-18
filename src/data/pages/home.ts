40f967ede306d9a5b99810a5382e432a62cf3010
Alexandre Pereira <pereira.aleandre@gmail.com>
2025-11-18 18:59:01 -0300

    Atualiza portfolio da home

--- src/data/pages/home.ts
+++ src/data/pages/home.ts
@@ -9,6 +9,9 @@ import img7 from "../../assets/images/portfolio/gravasteel-portfolio-17.webp";
 import img8 from "../../assets/images/portfolio/gravasteel-portfolio-08.webp";
 import img9 from "../../assets/images/portfolio/gravasteel-portfolio-09.webp";
 import img10 from "../../assets/images/portfolio/gravasteel-portfolio-10.webp";
+import img11 from "../../assets/images/portfolio/gravasteel-portfolio-19.webp";
+import img12 from "../../assets/images/portfolio/gravasteel-portfolio-14.webp";
+import img13 from "../../assets/images/portfolio/gravasteel-portfolio-12.webp";
 
 import logo from "../../assets/images/logo-gravasteel.webp";
 import IconWhatsapp from "../../assets/icons/brand-whatsapp.svg";
@@ -41,16 +44,19 @@ export const pageData = {
       title: "Projetos em Destaque",
       subtitle: "Veja alguns dos nossos melhores trabalhos realizados",
       items: [
-         { src: img1, alt: "Projeto 1" },
-         { src: img2, alt: "Projeto 2" },
-         { src: img3, alt: "Projeto 3" },
-         { src: img4, alt: "Projeto 4" },
-         { src: img5, alt: "Projeto 5" },
-         { src: img6, alt: "Projeto 6" },
-         { src: img7, alt: "Projeto 7" },
-         { src: img8, alt: "Projeto 8" },
-         { src: img9, alt: "Projeto 9" },
-         { src: img10, alt: "Projeto 10" },
+         { src: img1, alt: "Portfólio Gravasteel"},
+         { src: img2, alt: "Portfólio Gravasteel"},
+         { src: img3, alt: "Portfólio Gravasteel"},
+         { src: img4, alt: "Portfólio Gravasteel"},
+         { src: img5, alt: "Portfólio Gravasteel"},
+         { src: img6, alt: "Portfólio Gravasteel"},
+         { src: img7, alt: "Portfólio Gravasteel"},
+         { src: img8, alt: "Portfólio Gravasteel"},
+         { src: img9, alt: "Portfólio Gravasteel"},
+         { src: img10, alt: "Portfólio Gravasteel"},
+         { src: img11, alt: "Portfólio Gravasteel"},
+         { src: img12, alt: "Portfólio Gravasteel"},
+         { src: img13, alt: "Portfólio Gravasteel"},
       ],
    },
 
